5b08f9d3d15cdd3b584abd9accd7d0e3749177f5
Rohit Chopra <rohitchopra32@gmail.com>
2017-05-02 03:01:09 +0530

    Update production.py

--- GBPEC_PORTAL/settings/production.py
+++ GBPEC_PORTAL/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '_l*d3b)ioe*4!!ig8ro@fu@5-@3for0rbzgg5zrcg@y++(6la!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
